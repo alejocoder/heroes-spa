948b67fc2faeebe43c550de7ab17947410378988
alejandro <alejosamp@gmail.com>
2023-01-03 10:57:15 -0500

    filter, routing  and list components done

--- src/heroes/pages/DcPage.js
+++ src/heroes/pages/DcPage.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { HeroList } from '../components'
 
 export const DcPage = () => {
   return (
-    <h1>DcPage</h1>
+    <>
+      <h1>
+        DC Comics
+      </h1>
+      <hr></hr>
+
+      <HeroList publisher={'DC Comics'}/>
+    </>
   )
 }
